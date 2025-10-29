1bf143ce4210caa1767fe746bda3c561e03aba93
amandahagen <amandaibanezhagen@gmail.com>
2025-10-29 11:48:30 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Security.Cryptography;
+
+
+void ExibirMensagemDeBoasVindas()
+{
+    string mensagemDeBoasVindas = "Bem vindo ao JogoRpg!";
+    Console.WriteLine(mensagemDeBoasVindas);
+
+}
+
+void ExibirOpcoesDoMenu()
+{
+    Console.WriteLine("\nDigite 1 para escolha do jogador1");
+    Console.WriteLine("Digite 2 paraescolher o jogador2");
+    Console.WriteLine("Digite 3 para voltar");
+
+
+
+}
+ExibirMensagemDeBoasVindas();
+ExibirOpcoesDoMenu(); //chama a função no console
+
+
+Console.ReadLine();
